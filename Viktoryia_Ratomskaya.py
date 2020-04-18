d3581b003679fbe8eca07299e526a7c265418184
Vika Ratomskaya <ratom_v@mail.ru>
2020-04-18 21:18:41 +0300

    Add symbol \n in the end print

--- Viktoryia_Ratomskaya.py
+++ Viktoryia_Ratomskaya.py
@@ -1,29 +1,30 @@
-#____________________Основная часть____________________
+# Основная часть
 main_string = 'Не знаю, как там в Лондоне, я не была. Может, там собак\
 а — друг человека. А у нас управдом — друг человека!'
-print(f'Исходная строка: {main_string}')
-print(f'Количество символов - {len(main_string)}')
-print(f'Инверсная строка: {main_string[::-1]}')
-print(f'Все слова с большой буквы: {main_string.title()}')
-print(f'Весь текст прописными буквами: {main_string.upper()}')
-print('Число вхождений "нд" = {0}, "ам" = {1}, "o" = {2}'
+print(f'Исходная строка: {main_string}\n')
+print(f'Количество символов - {len(main_string)}\n')
+print(f'Инверсная строка: {main_string[::-1]}\n')
+print(f'Все слова с большой буквы: {main_string.title()}\n')
+print(f'Весь текст прописными буквами: {main_string.upper()}\n')
+print('Число вхождений "нд" = {0}, "ам" = {1}, "o" = {2}\n'
       .format(main_string.count('нд'), main_string.count('ам'),
               main_string.count('о')))
-#____________________Собственные упражнения____________________
-print('Строка заканчивается на "!" - {}'
+
+# Собственные упражнения
+print('Строка заканчивается на "!" - {}\n'
       .format(main_string.endswith('!')))
-print('Индекс первого упоминание элемента "a" = {}'
+print('Индекс первого упоминание элемента "a" = {}\n'
       .format(main_string.find('а')))
 print('Индекс первого упоминание подстроки "друг" начиная с 70 элемент\
-а исходной строки = {}'.format(main_string.index('друг', 70)))
-print(f'Все символы в строке строчные? - {main_string.islower()}')
-print('Каждый символ строки разделен пробелом: {}'
+а исходной строки = {}\n'.format(main_string.index('друг', 70)))
+print(f'Все символы в строке строчные? - {main_string.islower()}\n')
+print('Каждый символ строки разделен пробелом: {}\n'
       .format(' '.join(main_string)))
 dictionary = str.maketrans({'Л': 'L', 'д': 'd'})
-print('Замена символов "Л" на "L", "д" на "d": {}'
+print('Замена символов "Л" на "L", "д" на "d": {}\n'
       .format(main_string.translate(dictionary)))
-print('Замена слова "друг" на "friend": {}'
+print('Замена слова "друг" на "friend": {}\n'
       .format(main_string.replace('друг', 'friend')))
-print('Замена строчных букв на прописные и наоборот: {}'
+print('Замена строчных букв на прописные и наоборот: {}\n'
       .format(main_string.swapcase()))
 print(f'Все символы в строке строчные: {main_string.lower()}')
