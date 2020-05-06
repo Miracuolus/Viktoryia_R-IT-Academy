31757e6244dfa0bf0dfa6ac3144be3a252a307dd
Vika Ratomskaya <ratom_v@mail.ru>
2020-05-06 20:49:33 +0300

    Create class person

--- 04.05.2020/Ratomskaya_BMI_full.py
+++ 04.05.2020/Ratomskaya_BMI_full.py
@@ -1,3 +1,36 @@
+class person:
+    def __init__(self, second_name, name, father_name, height, mass, age):
+        self.__second_name = second_name
+        self.__name = name
+        self.__father_name = father_name
+        self.__height = height
+        self.__mass = mass
+        self.__age = age
+
+    def __str__(self):
+        format_string = '{} {} {}: рост = {}, вес = {}, возраст = {}'
+        return format_string.format(self.__second_name, self.__name,\
+            self.__father_name, self.__height, self.__mass, self.__age)
+        
+    def get_height(self):
+        return self.__height
+
+    def set_height(self, height):
+        self.__height = height
+
+    def get_mass(self):
+        return self.__mass
+
+    def set_mass(self, mass):
+        self.__mass = mass
+
+    def get_age(self):
+        return self.__age
+
+    def set_age(self, age):
+        self.__age = age
+        
+
 def get_bmi(mass, height):
     """Возвращает индекс массы тела"""
     return mass/(height/100)**2
