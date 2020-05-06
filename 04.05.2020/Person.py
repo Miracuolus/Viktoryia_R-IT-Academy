de79aa672174eaad36259f6e296e598606629993
Vika Ratomskaya <ratom_v@mail.ru>
2020-05-06 22:16:15 +0300

    Add three classes with Exception

--- 04.05.2020/Person.py
+++ 04.05.2020/Person.py
@@ -3,9 +3,9 @@ class Person:
         self.__second_name = second_name
         self.__name = name
         self.__father_name = father_name
-        self.__height = height
-        self.__mass = mass
-        self.__age = age
+        self.set_height(height)
+        self.set_mass(mass)
+        self.set_age(age)
 
     def __str__(self):
         format_string = '{} {} {}: рост = {}, вес = {}, возраст = {}'
@@ -25,16 +25,37 @@ class Person:
         return self.__height
 
     def set_height(self, height):
+        if height <= 0 or height >= 300:
+            raise InvalidHeight
         self.__height = height
 
     def get_mass(self):
         return self.__mass
 
     def set_mass(self, mass):
+        if mass <= 0 or mass >= 400:
+            raise InvalidMass
         self.__mass = mass
 
     def get_age(self):
         return self.__age
 
     def set_age(self, age):
+        if age < 0 or age > 150:
+            raise InvalidAge
         self.__age = age
+
+
+class InvalidHeight(Exception):
+    """Класс ошибки при вводе невалидного роста"""
+    pass
+
+
+class InvalidMass(Exception):
+    """Класс создания ошибки при вводе невалидной массы тела"""
+    pass
+
+
+class InvalidAge(Exception):
+    """Класс создания ошибки при вводе невалидного возраста"""
+    pass
