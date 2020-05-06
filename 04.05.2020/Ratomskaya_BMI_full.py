7cb671dfc447453056e789203a25bc45e4e2a9cf
Vika Ratomskaya <ratom_v@mail.ru>
2020-05-06 23:08:45 +0300

    Add function recomendation Added printing wrong numbers in fields Height, Mass, Age. Added function recomendation, which print recomendation for person.

--- 04.05.2020/Ratomskaya_BMI_full.py
+++ 04.05.2020/Ratomskaya_BMI_full.py
@@ -17,6 +17,25 @@ def print_scale(index):
     print(str(min_index) + '=' * left + '|' + '=' * right + \
         str(max_index))
 
+def recomendation(index):
+    """Рекомендации по результатам расчета ИМТ"""
+    if index < 16:
+        print(f'{person.get_name()}, у Вас выраженный дефицит массы тела')
+    elif 16 <= body_index <= 18.49:
+        print(f'{person.get_name()}, у Вас недостаточная масса тела')
+    elif 18.5 <= body_index <= 24.99:
+        print(f'{person.get_name()}, у Вас нормальный вес')
+    elif 25 <= body_index <= 29.99:
+        print(f'{person.get_name()}, у Вас предожирение')
+    elif 30 <= body_index <= 34.99:
+        print(f'{person.get_name()}, у Вас ожирение 1-ой степени')
+    elif 35 <= body_index <= 39.99:
+        print(f'{person.get_name()}, у Вас ожирение 2-ой степени')
+    elif body_index >= 40:
+        print(f'{person.get_name()}, у Вас ожирение 3-ой степени')
+    else:
+        print(f'{person.get_name()}, у Вас нормальный вес')
+
 exit = ''
 while exit.lower() != 'exit':
     family_name = input('Введите фамилию: ')
@@ -31,15 +50,16 @@ while exit.lower() != 'exit':
         body_index = get_bmi(mass, height)
         print(f'{person.get_name()}, Ваш индекс массы тела: {round(body_index, 2)}')
         print_scale(body_index)
+        recomendation(body_index)
     except ValueError:
         print('*Ошибка! В полях "Рост", "Вес", и "Возраст" необходимо'
             'вводить числовые значения')
     except InvalidHeight:
-        print('*Ошибка! Введите реальное значение роста')
+        print(f'*Ошибка! Некорректное значение роста: {height}')
     except InvalidMass:
-        print('*Ошибка! Введите реальное значение веса')
+        print(f'*Ошибка! Некорректное значение веса: {mass}')
     except InvalidAge:
-        print('*Ошибка! Введите реальное значение возраста')
+        print(f'*Ошибка! Некорректное значение возраста: {age}')
 
     exit = input('Если хотите выйти из программы, то введите exit: ')
 
