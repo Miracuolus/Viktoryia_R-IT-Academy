dadc739c0e4cafa3a9bfce9691cf1774f8cf3420
Vika Ratomskaya <ratom_v@mail.ru>
2020-05-06 21:32:54 +0300

    Replace class Person in file Person.py

---
+++ 04.05.2020/Person.py
@@ -0,0 +1,40 @@
+class Person:
+    def __init__(self, second_name, name, father_name, height, mass, age):
+        self.__second_name = second_name
+        self.__name = name
+        self.__father_name = father_name
+        self.__height = height
+        self.__mass = mass
+        self.__age = age
+
+    def __str__(self):
+        format_string = '{} {} {}: рост = {}, вес = {}, возраст = {}'
+        return format_string.format(self.__second_name, self.__name,\
+            self.__father_name, self.__height, self.__mass, self.__age)
+        
+    def get_second_name(self):
+        return self.__second_name
+    
+    def get_name(self):
+        return self.__name
+    
+    def get_father_name(self):
+        return self.__father_name
+
+    def get_height(self):
+        return self.__height
+
+    def set_height(self, height):
+        self.__height = height
+
+    def get_mass(self):
+        return self.__mass
+
+    def set_mass(self, mass):
+        self.__mass = mass
+
+    def get_age(self):
+        return self.__age
+
+    def set_age(self, age):
+        self.__age = age
