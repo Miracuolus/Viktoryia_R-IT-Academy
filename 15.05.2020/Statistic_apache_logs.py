2ca6fdb11a987760328ad6c2e4b57d436583091e
Vika Ratomskaya <ratom_v@mail.ru>
2020-05-11 23:02:48 +0300

    Created define numbers of unic IP-address, and print unic IP-adress

--- 15.05.2020/Statistic_apache_logs.py
+++ 15.05.2020/Statistic_apache_logs.py
@@ -1,8 +1,15 @@
 f = 'apache_logs.txt'
 with open(f, 'r') as fp:
     count = 0
+    ip_addresses = set()
     for line in fp.readlines():
         count += 1
+        ip = line.partition(' - - [')
+        ip_addresses.add(ip[0])
+
     print(f'Количество запросов в файле {f} = {count}')
+    print(f'Количество уникальных запросов в файле {f} = {len(ip_addresses)}')
+    print(f'Список уникальных запросов в файле {f}: {ip_addresses}')
+
 
     
