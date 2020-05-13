60227399829d159d93277cccf0b93f73312dab7a
Vika Ratomskaya <ratom_v@mail.ru>
2020-05-13 20:19:09 +0300

    Created parser Apache logs

--- 15.05.2020/Ratomskaya_statistic_apache_logs.py
+++ 15.05.2020/Ratomskaya_statistic_apache_logs.py
@@ -3,21 +3,21 @@ import re
 f = 'apache_logs.txt'
 brousers = {'Mozilla Firefox' : ['Gecko', 'Firefox'],
             'Mozilla Firefox2' : ['Gecko', 'BonEcho'],
-            'Mozilla for Linux' : ['Gecko', 'Firefox', 'Iceweasel'],
+            'Mozilla for Linux' : ['Gecko', 'Firefox', 'Iceweasel'], #94
             'Google Chrome': ['Chrome', 'Safari'],
             'Apple Safari': ['Version', 'Safari'],
-            'Opera 19': ['Chrome', 'Safari', 'OPR'],
-            'Opera 12': ['Presto', 'Version'],
-            'Opera 9': ['Presto'],
+            'Opera 19': ['Chrome', 'Safari', 'OPR'], #19
+            'Opera 12': ['Presto', 'Version'],#89
+            'Opera 9': ['Presto'], #89
             'Internet Explorer': ['like', 'Gecko'],
             'Microsoft Edge': ['Chrome', 'Safari', 'Edge'],
-            'Bing': ['BingPreview'],
-            'Chromium' : ['Ubuntu', 'Chromium', 'Chrome', 'Safari'],}
+            'Bing': ['BingPreview'], #8
+            'Chromium' : ['Ubuntu', 'Chromium', 'Chrome', 'Safari'],} #176
 
 mobile_brousers = {'Safari on iPhone': ['(iPhone', 'Version', 'Mobile', 'Safari'],
                    'Chrome on iPhone': ['(iPhone','CriOS', 'Mobile', 'Safari'],
-                   'Chrome on Android': ['Chrome', 'Mobile', 'Safari'],
-                   'Android': ['Version', 'Mobile', 'Safari'],
+                   'Chrome on Android': ['Android', 'Chrome', 'Mobile', 'Safari'],
+                   'Others on Android': ['Android', 'Version', 'Mobile', 'Safari'],
                    'Safari on iPad': ['(iPad', 'Version', 'Mobile', 'Safari'],
                    'Chrome on iPad': ['(iPad','CriOS', 'Mobile', 'Safari']}
 
@@ -25,25 +25,26 @@ search_systems = {'Яндекс': 'http://yandex.com/bots',
                   'Google': 'http://www.google.com/bot.html',
                   'Bing': 'http://www.bing.com/bingbot.htm',
                   'Yahoo! Slurp': 'http://help.yahoo.com/help/us/ysearch/slurp',
-                  'Mail.ru': 'http://go.mail.ru/help/robots',
-                  'Sputnik': 'http://corp.sputnik.ru/webmaster'}
+                  'Mail.ru': 'http://go.mail.ru/help/robots'}
 
 bots = {'Ahrefs': 'http://ahrefs.com/robot/',
         'Majestic': 'http://www.majestic12.co.uk/bot.php?+',
-        'SMTBot': 'http://www.similartech.com/smtbot',
-        'linkdex': 'http://www.linkdex.com/bots/',
         'Exabot': 'http://www.exabot.com/go/robot',
         'Heritrix': 'http://www.archive.org/details/archive.org_bot)'}
 
 count_brousers = {}
 for i in brousers.keys():
     count_brousers[i] = 0
-#for j in mobile_brousers.keys():
-#    count_brousers[j] = 0
-#for k in search_systems.keys():
-#    count_brousers[k] = 0
-#for g in bots.keys():
-#    count_brousers[g] = 0
+for j in mobile_brousers.keys():
+    count_brousers[j] = 0
+
+count_search = {}
+for k in search_systems.keys():
+    count_search[k] = 0
+
+count_bots = {}
+for g in bots.keys():
+    count_bots[g] = 0
 
 all_request_count = 0
 unic_ip_addresses = set()
@@ -65,6 +66,14 @@ def pattern_system():
     else:
         sys = re.split(r'\w+.+\"\s\"', system[1])
     return sys
+
+def pattern_agents():
+    agents = pattern_system()
+    if len(agents) == 2:
+       agent = re.split(r'\w+\/.+\s\(.+\)\s', agents[1]) 
+    else:
+       agent = re.split(r'\w+\/.+\s\(.+\)\s', agents[0])
+    return agent
 
 def unic_address(line):
     ip = pattern_unic_address.split(line)
@@ -109,6 +118,32 @@ def system():
         no_inform_syst += 1
     else:
         list_system.append(system[0])
+
+list_agent = []
+list_bots_research = []
+def agents():
+    global list_agent
+    agent = pattern_agents()
+    if len(agent) == 2:
+        agents = re.findall(r'\w+.+\n', agent[1]) # браузеры и поиск 
+        if re.findall(r'\w+\:\/+\w+\.\w+.+\n', agent[1]):
+            agents = re.findall(r'\w+\:\/+\w+\.\w+.+\n', agent[1])
+            list_bots_research.append(agents[0])
+            agents = re.split(r'\w+\:\/+\w+\.\w+.+\n', agent[1])
+            agents = re.split(r'\(.+', agent[1])
+            list_agent = agents[0].split(' ') # браузеры
+            return list_agent
+                        
+        else:
+            if agents != []:
+                list_agent = agents[0].split(' ') # браузеры
+                return list_agent
+
+    else:
+       agents = re.findall(r'\w+\:\/+\w+\.\w+.+\n', agent[0]) # боты
+       if agents != []:
+           list_bots_research.append(agents[0])
+           return list_agent
     
 
 def list_agents(line):
@@ -122,18 +157,17 @@ def list_agents(line):
     #print(line)
 
 request_agents = []
-def analysis(brousers, line):
-    request_agents = re.findall(r'^.+\).+\d+$', line)
-    #print(request_agents)
-    if False:
-        for key in brousers.keys():
-            is_find = True
-            for value in brousers.get(key):
-                if request_agents.find(value) == -1:
-                    is_find = False
-                    break
-            if is_find:    
-                count_brousers[key] = count_brousers.get(key) + 1
+def analysis(brousers, request_agents):
+    for key in brousers.keys():
+        if len(brousers.get(key)) != len(request_agents):
+            continue
+        is_find = True    
+        for agent in brousers.get(key):
+            if not find_agent(request_agents, agent):
+                is_find = False
+                break
+        if is_find:    
+            count_brousers[key] = count_brousers.get(key) + 1
 
 def analysis_mobile(brousers, line):
     for key in brousers.keys():
@@ -145,14 +179,14 @@ def analysis_mobile(brousers, line):
         if is_find:    
             count_brousers[key] = count_brousers.get(key) + 1
 
-def analysis_search_systems(brousers, line):
+def analysis_search_systems(count, brousers, line):
     for key in brousers.keys():
         is_find = True    
         if line.find(brousers[key]) == -1:
             is_find = False
             continue
         if is_find:    
-            count_brousers[key] = count_brousers.get(key) + 1
+            count[key] = count.get(key) + 1
 
                
      
@@ -179,8 +213,19 @@ with open(f, 'r') as fp:
         referer() # список URL-запросов
 
         system() # информация о системе
+
+        analysis_mobile(mobile_brousers, line)
+        analysis_search_systems(count_search,search_systems, line)
+        analysis_search_systems(count_bots,bots, line)
+
+        agent = agents()
+        if agent != None:
+            #print(agent)
+            l_count_del += 1
         
+        analysis(brousers, list_agent)
         
+        #print(len(list_bots_research))
         
         #agents = list_agents(line)
         #analysis_search_systems(search_systems, line)
@@ -196,8 +241,15 @@ with open(f, 'r') as fp:
 
             
         #    l_count_del += 1
-    #print(l_count_del)
-    #print(len(list_system))
+  
+    set_bots_research = set()
+    for i in list_bots_research:
+        bot = re.split(r'\"\n', i)
+        bot = re.split(r'\;', bot[0])
+        bot = re.split(r'\)', bot[0])
+        set_bots_research.add(bot[0])
+    #print(set_bots_research)
+    print(len(set_bots_research))
     print(f'Количество запросов в файле {f} = {all_request_count}')
     print(f'Количество уникальных запросов в файле {f} = {len(unic_ip)}')
     print(f'Нет URL-запроса: {not_url}')
@@ -212,6 +264,18 @@ with open(f, 'r') as fp:
     for key in count_brousers.keys():
         num += count_brousers.get(key)
     print(num)
+
+    print(count_search)
+    num2 = 0
+    for key in count_search.keys():
+        num2 += count_search.get(key)
+    print(num2)
+
+    print(count_bots)
+    num3 = 0
+    for key in count_bots.keys():
+        num3 += count_bots.get(key)
+    print(num3)
 
     
     
