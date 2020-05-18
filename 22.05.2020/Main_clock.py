8b54476a5404f4fc3898cf26c87f37f6cd222113
Vika Ratomskaya <ratom_v@mail.ru>
2020-05-19 01:27:53 +0300

    Created main cycle and changeg logic for enter

--- 22.05.2020/Main_clock.py
+++ 22.05.2020/Main_clock.py
@@ -1,15 +1,30 @@
 import datetime
 import time
+import os
 from Number import *
 
+
+#print('\u25A0'*20)
+#print('\u25A0')
+list_numbers = []
+for i in range(0, 10):
+    list_numbers.append(Number(i))
 while True:
     real_time = datetime.datetime.now()
+    h = real_time.hour
+    m = real_time.minute
+    s = real_time.second
     real_time = datetime.datetime.strftime(real_time, '%Y-%m-%d %H:%M:%S.%f')
     real_time = real_time[11: 19]
-    list_numbers = []
-    for number in real_time:
-        list_numbers.append(Number(number))
-    for value in range(0, len(list_numbers)):
-        print(list_numbers[value], end=' ')
-    print('\n')
-    time.sleep(1)
+    for count in range(0, Number.line):
+        print(list_numbers[h//10].get_line(count), end=' ')
+        print(list_numbers[h%10].get_line(count), end=' ')
+        print(' ', end=' ')
+        print(list_numbers[m//10].get_line(count), end=' ')
+        print(list_numbers[m%10].get_line(count), end=' ')
+        print(' ', end=' ')
+        print(list_numbers[s//10].get_line(count), end=' ')
+        print(list_numbers[s%10].get_line(count), end=' ')
+        print('')
+    time.sleep(0.2)
+    os.system('cls')  # очистка консоли
