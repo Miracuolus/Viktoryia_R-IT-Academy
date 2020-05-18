594e305bf7ececfffdac8cd43ebda07a5981c3fa
Vika Ratomskaya <ratom_v@mail.ru>
2020-05-18 22:53:24 +0300

    created enter time with delay 1 second

--- 22.05.2020/Main_clock.py
+++ 22.05.2020/Main_clock.py
@@ -1,6 +1,15 @@
 import datetime
+import time
+from Number import *
 
-real_time = datetime.datetime.now()
-
-real_time = datetime.datetime.strftime(real_time, '%Y-%m-%d %H:%M:%S.%f')
-real_time = real_time[11: 19]
+while True:
+    real_time = datetime.datetime.now()
+    real_time = datetime.datetime.strftime(real_time, '%Y-%m-%d %H:%M:%S.%f')
+    real_time = real_time[11: 19]
+    list_numbers = []
+    for number in real_time:
+        list_numbers.append(Number(number))
+    for value in range(0, len(list_numbers)):
+        print(list_numbers[value], end=' ')
+    print('\n')
+    time.sleep(1)
