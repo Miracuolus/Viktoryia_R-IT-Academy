9694d6d4ec4cbc3c7bad621b6f776fea86a2b94a
Vika Ratomskaya <ratom_v@mail.ru>
2020-05-19 19:50:54 +0300

    added print seporator

--- 22.05.2020/Number.py
+++ 22.05.2020/Number.py
@@ -6,15 +6,15 @@ class Number:
 
     def __init__(self, number):
         if number == 0:
-            self.list_number = [f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}',
+            self.list_number = ['{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol),
+                                '{0}{0}    {0}{0}'.format(self.symbol),
                                 f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
                                 f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
                                 f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
                                 f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
                                 f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
                                 f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
-                                f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
-                                f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}']
+                                '{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol)]
         if number == 1:
             self.list_number = [f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}   ',
                                 f'   {self.symbol}{self.symbol}   ',
@@ -24,87 +24,97 @@ class Number:
                                 f'   {self.symbol}{self.symbol}   ',
                                 f'   {self.symbol}{self.symbol}   ',
                                 f'   {self.symbol}{self.symbol}   ',
-                                f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}']
+                                '{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol)]
         if number == 2:
-            self.list_number = [f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}',
+            self.list_number = ['{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol),
                                 f'      {self.symbol}{self.symbol}',
                                 f'      {self.symbol}{self.symbol}',
                                 f'      {self.symbol}{self.symbol}',
-                                f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}',
+                                '{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol),
                                 f'{self.symbol}{self.symbol}      ',
                                 f'{self.symbol}{self.symbol}      ',
                                 f'{self.symbol}{self.symbol}      ',
-                                f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}']
+                                '{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol)]
         if number == 3:
-            self.list_number = [f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}',
+            self.list_number = ['{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol),
                                 f'      {self.symbol}{self.symbol}',
                                 f'      {self.symbol}{self.symbol}',
                                 f'      {self.symbol}{self.symbol}',
-                                f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}',
+                                '{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol),
                                 f'      {self.symbol}{self.symbol}',
                                 f'      {self.symbol}{self.symbol}',
                                 f'      {self.symbol}{self.symbol}',
-                                f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}']
+                                '{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol)]
         if number == 4:
             self.list_number = [f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
                                 f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
                                 f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
                                 f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
-                                f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}',
+                                '{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol),
                                 f'      {self.symbol}{self.symbol}',
                                 f'      {self.symbol}{self.symbol}',
                                 f'      {self.symbol}{self.symbol}',
                                 f'      {self.symbol}{self.symbol}']
         if number == 5:
-            self.list_number = [f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}',
+            self.list_number = ['{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol),
                                 f'{self.symbol}{self.symbol}      ',
                                 f'{self.symbol}{self.symbol}      ',
                                 f'{self.symbol}{self.symbol}      ',
-                                f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}',
+                                '{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol),
                                 f'      {self.symbol}{self.symbol}',
                                 f'      {self.symbol}{self.symbol}',
                                 f'      {self.symbol}{self.symbol}',
-                                f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}']
+                                '{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol)]
         if number == 6:
-            self.list_number = [f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}',
+            self.list_number = ['{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol),
                                 f'{self.symbol}{self.symbol}      ',
                                 f'{self.symbol}{self.symbol}      ',
                                 f'{self.symbol}{self.symbol}      ',
-                                f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}',
+                                '{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol),
                                 f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
                                 f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
                                 f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
-                                f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}']
+                                '{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol)]
         if number == 7:
-            self.list_number = [f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}',
+            self.list_number = ['{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol),
                                 f'      {self.symbol}{self.symbol}',
                                 f'      {self.symbol}{self.symbol}',
                                 f'      {self.symbol}{self.symbol}',
-                                f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}',
+                                '{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol),
                                 f'{self.symbol}{self.symbol}      ',
                                 f'{self.symbol}{self.symbol}      ',
                                 f'{self.symbol}{self.symbol}      ',
                                 f'{self.symbol}{self.symbol}      ']
         if number == 8:
-            self.list_number = [f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}',
+            self.list_number = ['{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol),
                                 f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
                                 f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
                                 f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
-                                f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}',
+                                '{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol),
                                 f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
                                 f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
                                 f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
-                                f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}']
+                                '{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol)]
         if number == 9:
-            self.list_number = [f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}',
+            self.list_number = ['{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol),
                                 f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
                                 f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
                                 f'{self.symbol}{self.symbol}    {self.symbol}{self.symbol}',
-                                f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}',
+                                '{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol),
                                 f'      {self.symbol}{self.symbol}',
                                 f'      {self.symbol}{self.symbol}',
                                 f'      {self.symbol}{self.symbol}',
-                                f'{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}{self.symbol}']
+                                '{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.symbol)]
+        if number == 10:
+            self.list_number = ['        ',
+                                '        ',
+                                '        ',
+                                f'   {self.symbol}{self.symbol}   ',
+                                '        ',
+                                f'   {self.symbol}{self.symbol}   ',
+                                '        ',
+                                '        ',
+                                '        ']
 
 
 
