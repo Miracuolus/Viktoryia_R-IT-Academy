a2e6f13c324aa2c06f3cb34bd346e81cd3adacb2
Vika Ratomskaya <ratom_v@mail.ru>
2020-05-19 21:28:54 +0300

    Created color print clock

--- 22.05.2020/Main_clock.py
+++ 22.05.2020/Main_clock.py
@@ -2,27 +2,31 @@ import datetime
 import time
 import os
 from Number import *
+import random
+import colorama
+from termcolor import colored
 
-
+colorama.init()
 list_numbers = []
 for i in range(0, 11):
     list_numbers.append(Number(i))
 while True:
+    style = random.randint(31, 36)
+    print('\33[' + str(style) + 'm')
+    print('\033[6m')
     real_time = datetime.datetime.now()
     h = real_time.hour
     m = real_time.minute
     s = real_time.second
-    real_time = datetime.datetime.strftime(real_time, '%Y-%m-%d %H:%M:%S.%f')
-    real_time = real_time[11: 19]
     for count in range(0, Number.line):
         print(list_numbers[h//10].get_line(count), end=' ')
         print(list_numbers[h%10].get_line(count), end=' ')
-        print(list_numbers[10].get_line(count), end=' ')  # разделитель
+        print('\33[6m', list_numbers[10].get_line(count), end=' ')  # разделитель
         print(list_numbers[m//10].get_line(count), end=' ')
         print(list_numbers[m%10].get_line(count), end=' ')
         print(list_numbers[10].get_line(count), end=' ')  # разделитель
         print(list_numbers[s//10].get_line(count), end=' ')
         print(list_numbers[s%10].get_line(count), end=' ')
         print('')
-    time.sleep(0.2)
+    time.sleep(1)
     os.system('cls')  # очистка консоли
